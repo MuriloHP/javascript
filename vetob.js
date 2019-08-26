4619a8297f4947751c7137256d09ba4426b82e8d
MURILO HENRIQUE PEREIRA <21894@unifacef.edu.br>
2019-08-26 20:33:44 -0300

    atualizando

--- vetob.js
+++ vetob.js
@@ -31,6 +31,22 @@ function calcula2(){
     //coloca objeto no veto
     vet.push(objeto)
     }
-    //calcula média
-
+    var media = 0
+    var somafilhos = 0
+    var maiorsalario = vet[0].salario
+    var sexof = 0
+    for(var i=0;i<vet.length;i++){
+        media = media + vet[i].salario
+        somafilhos = somafilhos + vet[i].filhos
+        if(vet[i].salario > maiorsalario){
+            maiorsalario = vet[i].salario
+        }
+        if((vet[i].sexo == "F") && (vet[i].salario > 1000)){
+            qtde++;
+        }
+    }
+    alert("Média" + media/vet.length)
+    alert("Filhos" + somafilhos/vet.length)
+    alert("Maior Salario" + maiorsalario)
+    alert("Percentual"  + qtde/3*100);
 }
